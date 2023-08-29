4df205dcdfb2a033c3c02198abee3827b6f78763
arifeensifat01 <arifeen.sifat01@gmail.com>
2023-08-29 16:07:28 +0600

    error page

--- error.vue
+++ error.vue
@@ -1,12 +1,14 @@
 <template>
-  <div>
-    
-  </div>
+    <div class="mt-7 max-w-sm mx-auto text-center card">
+     <p class="mt-7 text-7xl font-bold">{{ error.statucode }}</p>
+     <p class="mt-7 text-6xl">Ooops.</p>
+     <p class="mt-7">{{ error.message }}</p>
+    </div>
 </template>
 
 <script setup>
-
-
+   defineProps(['error'])
 </script>
+
 
 
